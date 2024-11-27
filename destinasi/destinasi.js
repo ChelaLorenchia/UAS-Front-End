beb3321c59a02dfdc0fc9f2c82f11c506c58be19
mayceren <161692576+mayceren@users.noreply.github.com>
2024-11-27 23:16:29 +0700

    update

--- destinasi/destinasi.js
+++ destinasi/destinasi.js
@@ -26,6 +26,30 @@ app.controller("MainController", function ($scope) {
         "https://thebalisun.com/wp-content/uploads/2024/08/Preservation-Efforts-To-Balis-Uluwatu-Temple-Will-Encourage-More-Visits.jpg",
       favorite: false,
     },
+    {
+      title: "Pantai Pasut",
+      description:
+        "pantai dengan suasana yang tenang dan damai sehingga sangat cocok untuk melakukan aktivitas meditasi",
+      image:
+        "https://lovebali.baliprov.go.id/storage/touriest_destinations/img-16631362876321721ff25b0.jpg",
+      favorite: false,
+    },
+    {
+      title: "Pantai Kuta",
+      description:
+        "pantai yang memiliki ombak yang tidak terlalu tinggi, sehingga cocok untuk berselancar atau melakukan aktivitas lainnya",
+      image:
+        "https://lovebali.baliprov.go.id/storage/touriest_destinations/img-16062842475fbdf3d73c888.jpg",
+      favorite: false,
+    },
+    {
+      title: "Uluwatu Temple",
+      description:
+        "terletak di tepi tebing yang megah, kuil menakjubkan ini memiliki nilai budaya lebih dari yang terlihat",
+      image:
+        "https://thebalisun.com/wp-content/uploads/2024/08/Preservation-Efforts-To-Balis-Uluwatu-Temple-Will-Encourage-More-Visits.jpg",
+      favorite: false,
+    },
   ];
 
   $scope.toggleFavorite = function (destination) {
